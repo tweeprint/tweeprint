d61388cc7562798c821661a79b3388433c57960a
Alex Harston <alex@harston.io>
2021-03-13 18:57:13 +0000

    updating website

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div className="App flex items-center justify-center h-screen">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-3xl mx-auto">
+          <h1 className="text-2xl font-bold mb-2 leading-7 text-gray-900 sm:text-3xl sm:truncate">
+            Tweeprint
+          </h1>
+          <h2 className="text-md font-bold mb-6 text-gray-400 sm:text-md sm:truncate">
+            Find and share cool twitter threads about preprints.
+          </h2>
               <Opener />
               <Footer />
           </div>
