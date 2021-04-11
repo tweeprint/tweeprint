9401ddc043775d30dabd1cc3eac223eed1544646
Alex Harston <alex@harston.io>
2021-04-12 00:16:42 +0100

    spacing categories and switching flex to grid layout on tweeprints

--- src/Category.js
+++ src/Category.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Category = ({category}) => {
     
     return (
-        <div className="p-2 my-1">
-            <Link className="px-2 py-2 hover:text-indigo-600 rounded text-xs" to={`/categories/${category.slug}`}>{`${category.category}`}</Link>
+        <div className="my-1">
+            <Link className="px-4 py-1 hover:text-indigo-600 rounded text-xs" to={`/categories/${category.slug}`}>{`${category.category}`}</Link>
         </div>
     );
 };
--- src/Tweeprints.js
+++ src/Tweeprints.js
@@ -8,7 +8,7 @@ const Tweeprints = ({tweeprints}) => {
             <div className="my-16" key={tweeprint.pk} >
                 <a href={`${tweeprint.fields.link}`}>
 
-                <div className="flex flex-col sm:flex-row items-center max-w-2xl">
+                <div className="flex flex-col sm:grid sm:grid-cols-4 items-center max-w-2xl">
                     <div className="flex flex-col">
                         <div className="flex flex-row justify-center">
                             <img className="w-16 h-16 rounded-full" src={tweeprint.fields.tweet_json.user.profile_image_url_https} alt=""/>
@@ -18,7 +18,7 @@ const Tweeprints = ({tweeprints}) => {
                             <p className="mx-2 text-sm">@{tweeprint.fields.tweet_json.user.screen_name}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col justify-content text-gray-600 text-left mx-4">
+                    <div className="flex flex-col sm:col-span-3 justify-content text-gray-600 text-left mx-4">
                         <h2 className="text-center sm:text-left text-indigo-400 font-bold">{tweeprint.fields.category}</h2>
                         <h2 className="text-lg text-center sm:text-left my-4">{tweeprint.fields.tweet_json.full_text}</h2>
                         <div className="flex flex-row text-sm justify-center sm:justify-start text-gray-400">
