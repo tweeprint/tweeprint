327ac2104d0528f04bb4f28c4dbe09bf9b44ebc3
Alex Harston <alex@harston.io>
2021-04-11 14:33:58 +0100

    adding mobile layout

--- src/Tweeprints.js
+++ src/Tweeprints.js
@@ -15,7 +15,9 @@ const Tweeprints = () => {
         <div className="">
         {tweeprints.map(tweeprint => 
             <div className="my-16" key={tweeprint.pk} >
-                <div className="flex flex-row justify-center items-center max-w-2xl">
+                <a href={`${tweeprint.fields.link}`}>
+
+                <div className="flex flex-col sm:flex-row items-center max-w-2xl">
                     <div className="flex flex-col">
                         <div className="flex flex-row justify-center">
                             <img className="w-16 h-16 rounded-full" src={tweeprint.fields.tweet_json.user.profile_image_url_https} alt=""/>
@@ -25,15 +27,16 @@ const Tweeprints = () => {
                             <p className="mx-2 text-sm">@{tweeprint.fields.tweet_json.user.screen_name}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col justify-content text-gray-600 text-left">
-                        <h2 className="text-left text-indigo-400">{tweeprint.fields.category}</h2>
-                        <h2 className="text-lg text-left my-4">{tweeprint.fields.tweet_json.full_text}</h2>
-                        <div className="flex flex-row">
-                            <p className="mx-4 text-sm">Likes: {tweeprint.fields.tweet_json.favorite_count}</p>
-                            <p className="mx-4 text-sm">Retweets: {tweeprint.fields.tweet_json.retweet_count}</p>
+                    <div className="flex flex-col justify-content text-gray-600 text-left mx-4">
+                        <h2 className="text-center sm:text-left text-indigo-400 font-bold">{tweeprint.fields.category}</h2>
+                        <h2 className="text-lg text-center sm:text-left my-4">{tweeprint.fields.tweet_json.full_text}</h2>
+                        <div className="flex flex-row text-sm justify-center sm:justify-start text-gray-400">
+                            <p className="mr-4">Likes: {tweeprint.fields.tweet_json.favorite_count}</p>
+                            <p className="mr-4">Retweets: {tweeprint.fields.tweet_json.retweet_count}</p>
                         </div>
                     </div>
                 </div>
+                </a>
             </div>
             )}
         </div>
